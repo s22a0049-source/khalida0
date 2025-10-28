016d5f55ac46233149504135c6e75a0b0bc0d40c
s22a0049-source <s22a0049@siswa.umk.edu.my>
2025-10-29 05:08:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,16 @@
 import streamlit as st
 
-st.ssn_page_config(
+st.set_page_config(
     page_title="Student Survey"
-}
-
-visualize = st.Page{'studentSurvey.py', title='Pencapaian Akademik Pelajar', icon-":material/school:")
+)
+visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik', icon=":material/school:")
 
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
 
 pg = st.navigation(
         {
-            "Menu": [home, visualize]
+            "Menu": [home, visualise]
         }
-    )
+    )
 
 pg.run()
